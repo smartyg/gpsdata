2c2defd4984290cf1dd478a958fbedfffa47f529
Martijn Goedhart <goedhart.martijn@gmail.com>
2022-08-07 16:54:49 +0200

    also include trait for GpsStatistics

---
+++ include/gpsdata/traits/GpsStatistics.hpp
@@ -0,0 +1,22 @@
+#ifndef _X_GPSDATA_TRAITS_GPSSTATISTICS_
+#define _X_GPSDATA_TRAITS_GPSSTATISTICS_
+
+#include <gpsdata/traits/common.hpp>
+#include <gpsdata/traits/GpsFactory.hpp>
+
+namespace gpsdata {
+	// Forward declaration of the GpsPoint type.
+	template<GpsFactoryTrait F>
+	class GpsStatistics;
+
+	inline namespace traits {
+		template<class T>
+		concept CONCEPT_RETURN GpsStatisticsTrait = requires(const T t) {
+			typename T::GpsFactory;
+			typename T::DataType;
+			std::is_base_of<GpsStatistics<typename T::GpsFactory>, T>::value;
+		};
+	}
+}
+
+#endif /* _X_GPSDATA_TRAITS_GPSSTATISTICS_ */
