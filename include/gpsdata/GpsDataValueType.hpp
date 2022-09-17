796daea1c2c063075fd8dcedc55299d90fe19bf2
Martijn Goedhart <goedhart.martijn@gmail.com>
2022-09-17 20:16:51 +0200

    GpsDataValueType: add comments explaining the types

--- include/gpsdata/GpsDataValueType.hpp
+++ include/gpsdata/GpsDataValueType.hpp
@@ -6,17 +6,17 @@
 namespace gpsdata {
 	enum GpsDataValueType : uint_fast8_t {
 		NONE_T = 0,
-		BOOL_T,
-		S_CHAR_T,
-		U_CHAR_T,
-		S_SHORT_T,
-		U_SHORT_T,
-		S_INT_T,
-		U_INT_T,
-		S_LONG_T,
-		U_LONG_T,
-		FLOAT_T,
-		DOUBLE_T
+		BOOL_T, // 1-bit value
+		S_CHAR_T, // 8-bits signed integer value
+		U_CHAR_T, // 8-bits unsigned integer value
+		S_SHORT_T, // 16-bits signed integer value
+		U_SHORT_T, // 16-bits unsigned integer value
+		S_INT_T, // 32-bits signed integer value
+		U_INT_T, // 32-bits unsigned integer value
+		S_LONG_T, // 64-bits signed integer value
+		U_LONG_T, // 64-bits unsigned integer value
+		FLOAT_T, // 32-bits floating number
+		DOUBLE_T // 8-bits floating number
 	};
 }
 
