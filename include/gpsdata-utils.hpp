6d53878dec01ba38bd812c44ab0c55b77a453e9c
Martijn Goedhart <goedhart.martijn@gmail.com>
2022-11-01 22:22:27 +0100

    use <> for inclusions in system headers

--- include/gpsdata-utils.hpp
+++ include/gpsdata-utils.hpp
@@ -1,9 +1,9 @@
 #ifndef _GPSDATA_UTILS_
 #define _GPSDATA_UTILS_
 
-#include "gpsdata/utils/Convert.hpp"
-#include "gpsdata/utils/GpsDataFactoryBasic.hpp"
-#include "gpsdata/utils/PointDate.hpp"
-#include "gpsdata/utils/ZoneDate.hpp"
+#include <gpsdata/utils/Convert.hpp>
+#include <gpsdata/utils/GpsDataFactoryBasic.hpp>
+#include <gpsdata/utils/PointDate.hpp>
+#include <gpsdata/utils/ZoneDate.hpp>
 
 #endif /* _GPSDATA_UTILS_ */
